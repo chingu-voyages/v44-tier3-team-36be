bd01a9391038ce55fcf411bcd139861635c76bee
Shawn Koong <shawnkoong@gmail.com>
2023-05-20 19:09:40 -0700

    making fetchData to scheduled task

--- main-app/src/main/java/com/nyctransittracker/mainapp/util/DataFetcher.java
+++ main-app/src/main/java/com/nyctransittracker/mainapp/util/DataFetcher.java
@@ -3,31 +3,28 @@ package com.nyctransittracker.mainapp.util;
 import com.nyctransittracker.mainapp.model.DataResponse;
 import com.nyctransittracker.mainapp.service.RedisService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
+@EnableScheduling
 @RequiredArgsConstructor
-public class DataFetcher implements CommandLineRunner {
+public class DataFetcher {
 
     private final WebClient webClient;
     private final static String url = "https://www.goodservice.io/api/routes/?detailed=1";
     private final RedisService redisService;
 
-    public DataResponse fetchData() {
+    @Scheduled(fixedRate = 30000)
+    public void fetchData() {
         DataResponse dataResponse = webClient.get()
                 .uri(url)
                 .retrieve()
                 .bodyToMono(DataResponse.class)
                 .block();
         redisService.saveData(dataResponse);
-        return dataResponse;
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        DataResponse response = fetchData();
-        System.out.println(response.getTimestamp());
-    }
 }
