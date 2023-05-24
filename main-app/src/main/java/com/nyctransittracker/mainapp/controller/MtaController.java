8f24fa5b5057aa2ee553eeec003aadec84069517
Shawn Koong <shawnkoong@gmail.com>
2023-05-24 15:03:37 -0700

    mta data controller

---
+++ main-app/src/main/java/com/nyctransittracker/mainapp/controller/MtaController.java
@@ -0,0 +1,21 @@
+package com.nyctransittracker.mainapp.controller;
+
+import com.nyctransittracker.mainapp.model.MtaResponse;
+import com.nyctransittracker.mainapp.service.RedisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/data")
+@RequiredArgsConstructor
+public class MtaController {
+
+    private final RedisService redisService;
+
+    @GetMapping
+    public MtaResponse getMtaData() {
+        return redisService.getData();
+    }
+}
