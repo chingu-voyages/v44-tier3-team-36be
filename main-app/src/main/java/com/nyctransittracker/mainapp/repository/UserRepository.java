b52678a29d96b072e8676c628403e52f17e74e49
Shawn Koong <shawnkoong@gmail.com>
2023-05-23 21:44:41 -0700

    updating planetscale username

--- main-app/src/main/java/com/nyctransittracker/mainapp/repository/UserRepository.java
+++ main-app/src/main/java/com/nyctransittracker/mainapp/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.nyctransittracker.mainapp.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface UserRepository extends JpaRepository<User, Integer>{
     Optional<User> findByEmail(String email);
 }
