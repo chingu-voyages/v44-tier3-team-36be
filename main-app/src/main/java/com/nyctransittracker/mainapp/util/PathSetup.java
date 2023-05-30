8a5943879fa168d78756c9ea6bdaaadf68d5811f
Shawn Koong <shawnkoong@gmail.com>
2023-05-29 19:20:43 -0700

    extracting path calculation to another service

--- main-app/src/main/java/com/nyctransittracker/mainapp/util/PathSetup.java
+++ main-app/src/main/java/com/nyctransittracker/mainapp/util/PathSetup.java
@@ -4,24 +4,15 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nyctransittracker.mainapp.model.*;
 import com.nyctransittracker.mainapp.service.PathService;
-import com.nyctransittracker.mainapp.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.LineString;
-import org.locationtech.jts.linearref.LengthIndexedLine;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 //@Component
 @RequiredArgsConstructor
@@ -29,14 +20,11 @@ import java.util.Map;
 public class PathSetup implements CommandLineRunner {
 
     private final PathService pathService;
-    private final RedisService redisService;
-    private final GeometryFactory geometryFactory;
     private final static String filePath = "classpath:station_details.json";
 
     @Override
     public void run(String... args) throws Exception {
 //        readJson();
-        calculateTrainPositions();
     }
 
     /*
@@ -74,54 +62,5 @@ public class PathSetup implements CommandLineRunner {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void calculateTrainPositions() {
-        MtaResponse mtaResponse = redisService.getData();
-        Map<String, Route> routes = mtaResponse.getRoutes();
-        List<Point> trainPositions = new ArrayList<>();
-        // testing for one line for now, change it to iterate through the map keys
-        Map.Entry<String, Route> routeEntry = routes.entrySet().iterator().next();
-        Route route = routeEntry.getValue();
-        Map<String, List<Trip>> trips = route.getTrips();
-        trips.forEach((direction, tripList) -> {
-            tripList.forEach((trip) -> {
-                String lastStopId = trip.getLastStopMade();
-                if (lastStopId == null) {
-                    return;
-                }
-                Map<String, Long> stops = trip.getStops();
-                String nextStopId = findNextStopId(stops, lastStopId);
-                String pathName = (direction.equals("north")) ?
-                        (lastStopId + "-" + nextStopId) : (nextStopId + "-" + lastStopId);
-                // simply getting from pathService will not work for express routes
-                Path path = pathService.getPath(pathName);
-                Point point = calculateTrainPosition(stops, path, lastStopId, nextStopId);
-                trainPositions.add(point);
-            });
-        });
-    }
-
-    public String findNextStopId(Map<String, Long> stops, String lastStopId) {
-        // sorting stops by the timestamp then extracting out the keys (stop IDs)
-        List<Map.Entry<String, Long>> entryList = new ArrayList<>(stops.entrySet());
-        entryList.sort(Map.Entry.comparingByValue());
-        List<String> stopIds = entryList.stream().map(Map.Entry::getKey).toList();
-        return stopIds.get(stopIds.indexOf(lastStopId) + 1); // not too sure if I have to check bounds here
-    }
-
-    public Point calculateTrainPosition(Map<String, Long> stops ,Path path, String lastStopId, String nextStopId) {
-        List<Point> points = path.getPoints();
-        Coordinate[] coordinates = points.stream()
-                .map(point -> new Coordinate(point.longitude(), point.latitude()))
-                .toArray(Coordinate[]::new);
-        LineString lineString = geometryFactory.createLineString(coordinates);
-        LengthIndexedLine indexedLine = new LengthIndexedLine(lineString);
-        long lastTimestamp = stops.get(lastStopId);
-        long nextTimeStamp = stops.get(nextStopId);
-        long nowTimestamp = Instant.now().getEpochSecond();
-        double progress = (double) (nowTimestamp - lastTimestamp) / (nextTimeStamp - lastTimestamp);
-        Coordinate coordinate = indexedLine.extractPoint(progress * lineString.getLength());
-        return new Point(coordinate.getX(), coordinate.getY());
     }
 }
